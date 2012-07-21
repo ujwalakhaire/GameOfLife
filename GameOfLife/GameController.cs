0a80aac4df022d51d39524fe048e7ede98c83029
Ujwala <ujwala.khaire@gmail.com>
2012-07-21 13:13:30 +0530

    First real test.

---
+++ GameOfLife/GameController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameOfLife
+{
+    public class GameController
+    {
+        public static void Play()
+        {
+        }
+    }
+}
--- GameOfLifeTests/Functional/GameOfLifeTest.cs
+++ GameOfLifeTests/Functional/GameOfLifeTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using GameOfLife;
 
 namespace GameOfLifeTests.Functional
 {
@@ -10,9 +11,9 @@ namespace GameOfLifeTests.Functional
     class GameOfLifeTest
     {
         [Test]
-        public void UltimateTruthTest()
+        public void BlockPatternTest()
         {
-            Assert.That(2 + 2, Is.EqualTo(4));
+            GameController.Play();
         }
     }
 }
