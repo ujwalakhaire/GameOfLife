b4865895f7b59c7e85a6d2b98c7f535c1f072f1a
Ujwala <ujwala.khaire@gmail.com>
2012-07-21 19:39:19 +0530

    Populating cells of board as list.

--- GameOfLife/Board.cs
+++ GameOfLife/Board.cs
@@ -5,13 +5,37 @@ using System.Text;
 
 namespace GameOfLife
 {
-    class Board
+    public class Board
     {
-        bool[,] cellData;
+        private bool[,] cellData;
+        private IList<Cell> cells;
+
+        public IList<Cell> Cells
+        {
+            get
+            {
+                return cells;
+            }
+        }
 
         public Board(bool[,] seedData)
         {
             cellData = seedData;
+            PopulateCells(seedData);
+        }
+
+        private void PopulateCells(bool[,] seedData)
+        {
+            cells = new List<Cell>();
+
+            int rowCount = seedData.GetUpperBound(0);
+            int columnCount = seedData.GetUpperBound(1);
+
+            for (int rowIndex = 0; rowIndex <= rowCount; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex <= columnCount; columnIndex++)
+                    cells.Add(new Cell());
+            }
         }
 
         internal bool[,] Show()
---
+++ GameOfLifeTests/Unit/BoardTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using GameOfLife;
+
+namespace GameOfLifeTests.Unit
+{
+    [TestFixture]
+    class BoardTest
+    {
+        [Test]
+        public void CellsCountOfSquareBoardTest()
+        {
+            bool[,] pattern = new bool[,]
+            {
+                {false, true, false},
+                {false, true, false},
+                {false, true, false}
+            };
+
+            Board board = new Board(pattern);
+
+            Assert.That(board.Cells.Count, Is.EqualTo(9));
+        }
+    }
+}
