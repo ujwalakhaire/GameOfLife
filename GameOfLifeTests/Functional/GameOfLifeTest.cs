4bf72d2360fbd81196675b66a51149340831a017
Ujwala <ujwala.khaire@gmail.com>
2012-07-21 13:48:51 +0530

    Added InputFormatter

--- GameOfLifeTests/Functional/GameOfLifeTest.cs
+++ GameOfLifeTests/Functional/GameOfLifeTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using NUnit.Framework;
 using GameOfLife;
+using GameOfLife.IO;
 
 namespace GameOfLifeTests.Functional
 {
@@ -13,6 +14,8 @@ namespace GameOfLifeTests.Functional
         [Test]
         public void BlockPatternTest()
         {
+            IInputFormatter inputter = new InMemoryInputFormatter();
+
             GameController.Play();
         }
     }
