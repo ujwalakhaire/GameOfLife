0cfb1d14238ccf3d4de57140b651cdfd6945f129
Ujwala <ujwala.khaire@gmail.com>
2012-07-21 12:34:54 +0530

    First working test.

---
+++ GameOfLifeTests/Functional/GameOfLifeTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace GameOfLifeTests.Functional
+{
+    [TestFixture]
+    class GameOfLifeTest
+    {
+        [Test]
+        public void UltimateTruthTest()
+        {
+            Assert.That(2 + 2, Is.EqualTo(4));
+        }
+    }
+}
